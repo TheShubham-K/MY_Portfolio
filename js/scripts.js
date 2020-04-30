cb4b90214936e3a5a5dbbfbca143416dba927736
shubham-sys <subham.kumar032@gmail.com>
2020-04-30 14:59:46 +0530

    Added css's & js's folders

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $("#mycarousel").carousel({
+        interval: 2000,
+    });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+            $("#mycarousel").carousel("pause");
+            $("#carouselButton").children("span").removeClass("fa-pause");
+            $("#carouselButton").children("span").addClass("fa-play");
+        } else if (
+            $("#carouselButton").children("span").hasClass("fa-play")
+        ) {
+            $("#mycarousel").carousel("cycle");
+            $("#carouselButton").children("span").removeClass("fa-play");
+            $("#carouselButton").children("span").addClass("fa-pause");
+        }
+    });
+
+
+    $("#myEduModalToggle").click(function () {
+        $("#educationModal").modal("toggle");
+    });
+
+
+    $("#mydownload").click(function () {
+        $("#mydownloadModal").modal("toggle");
+    });
+});
